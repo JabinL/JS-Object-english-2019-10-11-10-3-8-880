f257e13b326aeeb7292904f3e9cab4d76fb0c96b
Jabin <2099008974@qq.com>
2020-06-16 15:50:46 +0800

    finished

---
+++ mian.js
@@ -0,0 +1,17 @@
+//1
+
+var user = {};
+user.name= 'John';
+user.surname = 'Mike';
+user.name = 'Peter';
+delete user.name;
+
+//2
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+
+var sum = fruit.apple+fruit.pear+fruit.peach;
+console.log(sum);
